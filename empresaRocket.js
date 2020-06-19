649df7cc552d8567ac58ceab0cca07211c3f2009
saramargarido <saramargarido@gmail.com>
2020-06-19 19:16:26 -0300

    desafio dados de uma empresa, objetos e vetores

---
+++ empresaRocket.js
@@ -0,0 +1,11 @@
+const empresa = {
+    nome: 'Rocketseat',
+    cor: 'Roxo',
+    foco: 'Programação',
+    endereco: {
+        rua: 'Rua Guilherme Gembala',
+        numero: 206
+    }
+}
+
+console.log (`A empresa ${empresa.nome} está localizada na ${empresa.endereco.rua}, ${empresa.endereco.numero}`)
